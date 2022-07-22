1d248ef2425ca8642f8e0de8f91cf24ec402adc6
Richard Price <richyprice77@gmail.com>
2022-07-22 09:10:16 +0100

    inital commit

---
+++ SpanishPhrases/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpanishPhrases
+//
+//  Created by Richard Price on 22/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpanishPhrases/SpanishPhrasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpanishPhrasesApp.swift
+//  SpanishPhrases
+//
+//  Created by Richard Price on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpanishPhrasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

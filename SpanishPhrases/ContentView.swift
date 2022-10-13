6a79c866905590e24d867bda728f091c601f87ed
Richard Price <richyprice77@gmail.com>
2022-10-13 19:26:36 +0100

    inital setup imported assets setup top header with search bar

--- SpanishPhrases/ContentView.swift
+++ SpanishPhrases/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //step 3 call home struct
+        Home()
     }
 }
 
@@ -19,3 +19,70 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+//step 2 create a home struct and a top header
+struct Home: View {
+    //step 3 state var for the search bar txt
+    @State private var txt = ""
+    var body: some View {
+        VStack {
+            
+            //MARK: TOP HEADER------------------
+            HStack {
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    Text("Hello, Richard").font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text("welcome to Learn Phrase")
+                }
+                .foregroundColor(.black)
+                Spacer(minLength: 0)
+                Button(action: {}) {
+                    Image("AnimalsResized")
+                        .resizable()
+                        .renderingMode(.original)
+                        .frame(width: 50, height: 50)
+                }
+            }.padding()
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    //Step 4 create search bar
+                    //MARK: SEARCH BAR---------------
+                    HStack(spacing: 15) {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Search Categories", text: $txt)
+                    }
+                    .padding(.vertical, 15)
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    .clipShape(Capsule())
+                    
+                    //Step 5 categories section header
+                    //MARK: CATEGORIES
+                    
+                    
+                    
+                    
+                }
+                .padding()
+            }
+        }
+        .background(Color.black.opacity(0.05).ignoresSafeArea(.all, edges: .all))
+    }
+}
+//step 1 create some dummy data, after importing assets
+struct Category: Identifiable {
+    var id = UUID().uuidString
+    var name: String
+    var catNumber: Int
+    var asset: String
+}
+
+var categories = [
+        Category(name: "Animals", catNumber: 1, asset: "AnimalsResized"),
+        Category(name: "Weather", catNumber: 2, asset: "AnimalsResized"),
+        Category(name: "Sports", catNumber: 3, asset: "AnimalsResized"),
+        Category(name: "Professional", catNumber: 4, asset: "AnimalsResized"),
+]
